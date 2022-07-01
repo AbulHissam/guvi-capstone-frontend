36c109fc90bbc939d256aee835531c2a25286473
AbulHissam <abulhissam009@gmail.com>
2022-07-01 21:41:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with Create React App
+
+GUVI capstone project
+Capacity planning for an office
+
+yarn is the package manager
+
+TO run this project
+
+1.`yarn install`
+
+2.`yarn build`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Capacity Planning for an Office - GUVI Capstone Project</p>
+    </div>
+  );
+}
+
+export default App;
